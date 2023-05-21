3f7adb15ad05651d44ac97966e5530128d64a979
Pavel Kirilin <win10@list.ru>
2023-05-21 14:44:05 +0400

    Fixed aiohttp app.
    Signed-off-by: Pavel Kirilin <win10@list.ru>

--- aiohttp-app/tests/test_handlers.py
+++ aiohttp-app/tests/test_handlers.py
@@ -6,8 +6,8 @@ from aiohttp.test_utils import TestClient
 
 
 @pytest.mark.anyio
-async def test_health(test_client: TestClient):
-    """Test that healthcheck is working."""
+async def test_handlers(test_client: TestClient):
+    """Test that handlers are working."""
     put_resp = await test_client.put("/objects", json={"name": "pupa"})
     assert put_resp.status == HTTPStatus.OK
     response = await test_client.get("/objects")
